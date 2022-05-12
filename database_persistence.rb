db84a7a942dfd8460a586b504b3a9994735fcbef
Salo M <salofb21410@gmail.com>
2022-05-11 17:22:17 -0700

    sql fixes

--- database_persistence.rb
+++ database_persistence.rb
@@ -21,24 +21,32 @@ class DatabasePersistence
   end
 
   def find_list(id)
-    sql = "SELECT * FROM lists WHERE id = $1"
+    sql = <<~SQL
+      SELECT lists.*, COUNT(todo.id) AS todos_count,
+        COUNT(NULLIF(todo.completed, true)) AS todos_remaining_count
+        FROM lists
+        LEFT JOIN todo ON todo.list_id = lists.id
+        WHERE lists.id = $1
+        GROUP BY lists.id
+        ORDER BY lists.name;
+    SQL
     result = query(sql, id)
-
-    tuple = result.first
-    list_id = tuple["id"].to_i
-    todos = find_todos_for_list(list_id)
-
-    { id: list_id, name: tuple["name"] , todos: todos }
+    tuple_to_list_hash(result.first)
   end
 
   def all_lists
-    sql = "SELECT * FROM lists;"
+    sql = <<~SQL
+      SELECT lists.*, COUNT(todo.id) AS todos_count,
+        COUNT(NULLIF(todo.completed, true)) AS todos_remaining_count
+        FROM lists
+        LEFT JOIN todo ON todo.list_id = lists.id
+        GROUP BY lists.id
+        ORDER BY lists.name;
+    SQL
     result = query(sql)
 
     result.map do |tuple|
-      list_id = tuple["id"].to_i
-      todos = find_todos_for_list(list_id)
-      {id: list_id, name: tuple["name"] , todos: todos}
+      tuple_to_list_hash(tuple)
     end
   end
 
@@ -102,8 +110,6 @@ class DatabasePersistence
     @db.close
   end
 
-  private
-
   def find_todos_for_list(list_id)
     todo_sql = "SELECT * FROM todo WHERE list_id = $1"
     todos_result = query(todo_sql, list_id)
@@ -113,5 +119,17 @@ class DatabasePersistence
         name: todo_tuple["name"] ,
         completed: todo_tuple["completed"] == "t" }
       end
+  end
+
+  private
+
+  def tuple_to_list_hash(tuple)
+
+
+    { id: tuple["id"].to_i,
+      name: tuple["name"],
+      todos_count: tuple["todos_count"].to_i,
+      todos_remaining_count: tuple["todos_remaining_count"].to_i
+    }
   end
 end
--- todo.rb
+++ todo.rb
@@ -1,7 +1,7 @@
 require "sinatra"
 require "sinatra/content_for"
 require "tilt/erubis"
-
+require "pry"
 require_relative "database_persistence"
 
 configure do
@@ -17,20 +17,21 @@ end
 
 helpers do
   def list_complete?(list)
-    todos_count(list) > 0 && todos_remaining_count(list) == 0
+    # todos_count(list) > 0 && todos_remaining_count(list) == 0
+    list[:todos_count] > 0 && list[:todos_remaining_count] == 0
   end
 
   def list_class(list)
     "complete" if list_complete?(list)
   end
 
-  def todos_count(list)
-    list[:todos].size
-  end
+  # def todos_count(list)
+  #   list[:todos].size
+  # end
 
-  def todos_remaining_count(list)
-    list[:todos].count { |todo| !todo[:completed] }
-  end
+  # def todos_remaining_count(list)
+  #   list[:todos].count { |todo| !todo[:completed] }
+  # end
 
   def sort_lists(lists, &block)
     complete_lists, incomplete_lists = lists.partition { |list| list_complete?(list) }
@@ -116,6 +117,7 @@ end
 get "/lists/:id" do
   @list_id = params[:id].to_i
   @list = load_list(@list_id)
+  @todos = @storage.find_todos_for_list(@list_id)
   erb :list, layout: :layout
 end
 
